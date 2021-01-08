5c53d3a9dfccea080a5782786109a509be656394
mmartin4972 <mmartin4972@gmail.com>
2021-01-08 13:15:39 -0500

    A possible fix

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -34,6 +34,6 @@ cd singularity-$VERSION
 make
 sudo make install
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=10-CFtvKabLEEUe7j7kCCaL2_RQ53gOm3' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=10-CFtvKabLEEUe7j7kCCaL2_RQ53gOm3" -O perceptionDev.sif && rm -rf /tmp/cookies.txt
+
 
 #git clone https://github.com/umrover/mrover-workspace.git
