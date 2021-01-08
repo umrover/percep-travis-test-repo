e8d282202c55ca04d40540a325e73819ec40a580
mmartin4972 <mmartin4972@gmail.com>
2021-01-08 12:49:12 -0500

    Test3

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -13,19 +13,20 @@ echo "sregistry Version:"
 
 # Install Singularity
 
-SINGULARITY_BASE="${GOPATH}/src/github.com/sylabs/singularity"
-export PATH="${GOPATH}/bin:${PATH}"
+#SINGULARITY_BASE="${GOPATH}/src/github.com/sylabs/singularity"
+#export PATH="${GOPATH}/bin:${PATH}"
 
-mkdir -p "${GOPATH}/src/github.com/sylabs"
-cd "${GOPATH}/src/github.com/sylabs"
+#mkdir -p "${GOPATH}/src/github.com/sylabs"
+#cd "${GOPATH}/src/github.com/sylabs"
 
-git clone -b release-3.2 https://github.com/sylabs/singularity
-cd singularity
-./mconfig -v -p /usr/local
-make -j `nproc 2>/dev/null || echo 1` -C ./builddir all
-sudo make -C ./builddir install
+#git clone -b release-3.2 https://github.com/sylabs/singularity
+#cd singularity
+#./mconfig -v -p /usr/local
+#make -j `nproc 2>/dev/null || echo 1` -C ./builddir all
+#sudo make -C ./builddir install
 
 # Install Singularity again
+VERSION=2.5.2
 wget https://github.com/singularityware/singularity/releases/download/$VERSION/singularity-$VERSION.tar.gz
 tar xvf singularity-$VERSION.tar.gz
 cd singularity-$VERSION
