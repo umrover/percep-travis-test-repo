eb6dddcf506d01bd8ec5da94e883121cf8dbb1b4
mmartin4972 <mmartin4972@gmail.com>
2021-01-10 00:34:20 -0500

    Failed for a reason I don't know why

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -12,25 +12,7 @@ sregistry version
 echo "sregistry Version:"
 
 # Install Singularity
-
-#SINGULARITY_BASE="${GOPATH}/src/github.com/sylabs/singularity"
-#export PATH="${GOPATH}/bin:${PATH}"
-
-#mkdir -p "${GOPATH}/src/github.com/sylabs"
-#cd "${GOPATH}/src/github.com/sylabs"
-
-#git clone -b release-3.2 https://github.com/sylabs/singularity
-#cd singularity
-#./mconfig -v -p /usr/local
-#make -j `nproc 2>/dev/null || echo 1` -C ./builddir all
-#sudo make -C ./builddir install
-
-# Install Singularity
 export VERSION=3.7.0 &&
     wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz && \
     tar -xzf singularity-${VERSION}.tar.gz && \
     cd singularity
-
-#Download Image From AWS
-
-#git clone https://github.com/umrover/perception-dev-container.git
