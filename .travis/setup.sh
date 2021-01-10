a4fba87190a143b6fdfd8edf129f40935cba3c92
mmartin4972 <mmartin4972@gmail.com>
2021-01-10 18:33:19 -0500

    Added extra GO installation instructions

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -10,6 +10,16 @@ pip install --user sregistry[all]
 sregistry version
 
 echo "sregistry Version:"
+#Install Go
+export VERSION=1.13.5 OS=linux ARCH=amd64 && \
+    wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
+    sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \
+    rm go$VERSION.$OS-$ARCH.tar.gz
+    
+echo 'export GOPATH=${HOME}/go' >> ~/.bashrc && \
+    echo 'export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin' >> ~/.bashrc && \
+    source ~/.bashrc
+
 
 # Install Singularity
 export VERSION=3.7.0 && \
@@ -22,3 +32,4 @@ export VERSION=3.7.0 && \
     sudo make -C ./builddir install
     
     . /usr/local/etc/bash_completion.d/singularity
+        
