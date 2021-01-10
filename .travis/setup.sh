bf540f8834ca4531a9a16d19306f4fcd2190cc73
mmartin4972 <mmartin4972@gmail.com>
2021-01-10 18:34:05 -0500

    modified how GO was updated

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -11,14 +11,14 @@ sregistry version
 
 echo "sregistry Version:"
 #Install Go
-export VERSION=1.13.5 OS=linux ARCH=amd64 && \
-    wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
-    sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \
-    rm go$VERSION.$OS-$ARCH.tar.gz
-    
-echo 'export GOPATH=${HOME}/go' >> ~/.bashrc && \
-    echo 'export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin' >> ~/.bashrc && \
-    source ~/.bashrc
+#export VERSION=1.13.5 OS=linux ARCH=amd64 && \
+#    wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
+#    sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \
+#    rm go$VERSION.$OS-$ARCH.tar.gz
+
+#echo 'export GOPATH=${HOME}/go' >> ~/.bashrc && \
+#    echo 'export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin' >> ~/.bashrc && \
+#    source ~/.bashrc
 
 
 # Install Singularity
