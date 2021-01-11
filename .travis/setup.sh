ce47d0e360041595602e3a8861806cfc44108e45
mmartin4972 <mmartin4972@gmail.com>
2021-01-11 12:07:24 -0500

    Added helpful output:

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -45,6 +45,8 @@ export VERSION=3.5.2 && \
     ./mconfig && \
     make -C ./builddir && \
     sudo make -C ./builddir install
+    echo "stuff has been installed stop travis stop"
+    pwd
 
 #wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=10-CFtvKabLEEUe7j7kCCaL2_RQ53gOm3' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=10-CFtvKabLEEUe7j7kCCaL2_RQ53gOm3" -O perceptionDev.sif && rm -rf /tmp/cookies.txt
 
