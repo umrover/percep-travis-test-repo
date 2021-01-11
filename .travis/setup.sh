4c36d50e2be29a6a35096d4052c6961ae5a06b46
mmartin4972 <mmartin4972@gmail.com>
2021-01-11 10:30:23 -0500

    Might work

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -11,14 +11,10 @@ sregistry version
 
 echo "sregistry Version:"
 #Install Go
-export VERSION=1.13.5 OS=linux ARCH=amd64 && \
-    wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
-    sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \
-    rm go$VERSION.$OS-$ARCH.tar.gz
-
-echo 'export GOPATH=${HOME}/go' >> ~/.bashrc && \
-    echo 'export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin' >> ~/.bashrc && \
-    source ~/.bashrc
+wget https://golang.org/dl/go1.15.6.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.15.6.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+go version
 
 
 # Install Singularity
